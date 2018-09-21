c134a34f472f1659e4769556427e64efc2aa60e5
manyuemeicha <1406096402@qqc.om>
2018-09-21 17:50:04 +0800

    类里的方法之间加了空行，否则总是编辑器总是划线

--- runtest.py
+++ runtest.py
@@ -8,7 +8,10 @@ from src.common import send_mail
 fp="./report/report.html"
 pytest.main(["-s","./src/test_case/","--html="+fp,"--self-contained-html"])
 #pytest命令行执行默认不会打印log信息，需要加‘-s’参数或者 ‘–capture=no’，即pytest -s
-#--self-contained-html  不加这个参数的话 发送邮件的报告附件里没有样式
+#--html=**生成的报告，css是独立的，分享报告的时候样式会丢失，为了更好的分享发邮件展示报告，
+# 可以把css样式合并到html里，即在pytest.main()里加上--self-contained-html参数
+
+
 #send_mail.send_mail(fp)
 
 
